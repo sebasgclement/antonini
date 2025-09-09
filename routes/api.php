0914d58b37ee96684c4376392ca8c923acb56b3e
Sebastian Clement <sebastian.clement@unraf.edu.ar>
2025-09-09 09:19:38 -0300

    backend ajustado y funcional

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
 
-Route::post('/auth/login', [AuthController::class,'login']);
-Route::post('/auth/logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::post('/customers/register', [CustomerController::class,'register']);
+Route::post('/customers/register', [CustomerController::class, 'register']);
+
+Route::get('/ping', fn () => response()->json(['pong' => true]));
