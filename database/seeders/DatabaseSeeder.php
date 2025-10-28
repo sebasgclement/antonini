1424d8147099681101158afccab43cb910b0eb69
Sebastián Clement <sebasclement.sc@gmail.com>
2025-10-27 23:37:36 -0300

    cambio saldos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,19 +8,21 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
-    {
-        // Usuario de prueba
-        User::factory()->create([
-            'name'  => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+{
+    // Usuario de prueba
+    User::factory()->create([
+        'name'  => 'Test User',
+        'email' => 'test@example.com',
+    ]);
 
-        // Ejecutar todos los seeders
-        $this->call([
-            RoleSeeder::class,
-            CustomerSeeder::class,
-            VehicleSeeder::class,
-            VehicleExpenseSeeder::class,
-        ]);
-    }
+    // Ejecutar todos los seeders
+    $this->call([
+        RoleSeeder::class,
+        CustomerSeeder::class,
+        VehicleSeeder::class,
+        VehicleExpenseSeeder::class,
+        AdminUserSeeder::class,
+    ]);
+}
+
 }
